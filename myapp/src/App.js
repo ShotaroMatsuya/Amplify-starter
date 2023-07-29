4c3796e6df9f4de9c5299d63fe20261ef6a7057a
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-07-29 12:38:49 +0900

    feat: show todos

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,6 +2,12 @@ import { DataStore } from '@aws-amplify/datastore';
 import { Todo } from './models';
 
 function App() {
+
+  async function showTodos() {
+    const models = await DataStore.query(Todo);
+    console.log(models);
+  }
+
   async function addTodo() {
     await DataStore.save(
       new Todo({
@@ -16,6 +22,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <button onClick={addTodo}><h1>Add Todo</h1></button>
+        <button onClick={showTodos}><h1>Show Todos</h1></button>
       </header>
     </div>
   );
