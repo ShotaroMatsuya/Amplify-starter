438d703f5a337e1dca4101e14c52a498eccc237d
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-07-29 12:38:30 +0900

    feat: create todo

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,9 +2,20 @@ import { DataStore } from '@aws-amplify/datastore';
 import { Todo } from './models';
 
 function App() {
+  async function addTodo() {
+    await DataStore.save(
+      new Todo({
+      "name": "Lorem ipsum dolor sit amet",
+      "description": "Lorem ipsum dolor sit amet"
+    })
+  );
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={addTodo}><h1>Add Todo</h1></button>
       </header>
     </div>
   );
