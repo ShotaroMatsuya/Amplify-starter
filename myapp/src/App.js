08597b8f72f5f04b3ba22faa9bbc7c4a14bb892a
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-07-29 12:38:09 +0900

    chore: setup demo app

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import { DataStore } from '@aws-amplify/datastore';
+import { Todo } from './models';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
