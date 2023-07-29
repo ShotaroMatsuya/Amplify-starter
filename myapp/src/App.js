4bc85267d2b60c60b4ac4f59acd56a2f6b5a0777
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-07-29 12:39:06 +0900

    feat: update todo

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,6 +3,16 @@ import { Todo } from './models';
 
 function App() {
 
+  async function update() {
+    const original = await DataStore.query(Todo, 'fd22d3a0-f9cb-4919-90a2-ef8b0bd3880c');
+    await DataStore.save(Todo.copyOf(original, item => {
+        // Update the values on {item} variable to update DataStore entry
+        item.name = `title ${Date.now()}`;
+    }));
+
+  }
+
+
   async function showTodos() {
     const models = await DataStore.query(Todo);
     console.log(models);
@@ -23,6 +33,7 @@ function App() {
       <header className="App-header">
         <button onClick={addTodo}><h1>Add Todo</h1></button>
         <button onClick={showTodos}><h1>Show Todos</h1></button>
+        <button onClick={update}><h1>Update</h1></button>
       </header>
     </div>
   );
